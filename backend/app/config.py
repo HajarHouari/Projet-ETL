ceefda256b1e197b1edcebbbcb356ba2f20b8816
7Nayy <nbenkrouidel78@gmail.com>
2026-02-10 11:52:46 +0100

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,12 +2,12 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DATA_PATH = os.getenv("DATA_PATH")
+# Dossier pour le CSV (fixe dans l'image Docker)
+RAW_DATA_PATH = os.path.join(BASE_DIR, "raw_data") 
+CSV_PATH = os.path.join(RAW_DATA_PATH, "dataset_cafe.csv")
 
-if DATA_PATH is None:
-    DATA_PATH = os.path.join(BASE_DIR, "data")
-
-CSV_PATH = os.path.join(DATA_PATH, "dataset_cafe.csv")
+# Dossier pour la DB (monté via le volume Kubernetes)
+DATA_PATH = os.getenv("DATA_PATH", os.path.join(BASE_DIR, "data"))
 DB_PATH = os.getenv("SQLITE_PATH", os.path.join(DATA_PATH, "app.db"))
 
 DATABASE_URL = f"sqlite:///{DB_PATH}"
